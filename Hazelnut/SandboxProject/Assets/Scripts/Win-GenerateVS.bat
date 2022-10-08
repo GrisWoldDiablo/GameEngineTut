0db1f9ddcf892c1f7e73c9b68379f8c47c377221
GrisWoldDiablo <griswoldd@hotmail.com>
2022-10-07 21:17:31 -0400

    Fixed batch file to generate Sanbox Solution

--- Hazelnut/SandboxProject/Assets/Scripts/Win-GenerateVS.bat
+++ Hazelnut/SandboxProject/Assets/Scripts/Win-GenerateVS.bat
@@ -1,6 +1,5 @@
 @echo off
 echo --- Generating Sandbox Solution ---
-pushd %~dp0\..\..\..\..\
-call vendor\premake\bin\premake5.exe vs2022
+call ..\..\..\..\vendor\premake\bin\premake5.exe vs2022
 popd
 PAUSE
