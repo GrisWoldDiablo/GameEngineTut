f44c58ea023c953d415cea7e89f1aaa42d11c22b
GrisWold Diablo <griswoldd@hotmail.com>
2023-06-04 21:53:25 -0400

    Fix IsNearlyZero and IsNearlyEqual
    ~ Multiplying Epsilons was generating to small of a number they should be added.

--- Hazel-ScriptCore/Source/Hazel/Extension/DoubleExtension.cs
+++ Hazel-ScriptCore/Source/Hazel/Extension/DoubleExtension.cs
@@ -4,12 +4,12 @@
 	{
 		public static bool IsNearlyZero(this double value)
 		{
-			return System.Math.Abs(value) < double.Epsilon * double.Epsilon;
+			return System.Math.Abs(value) < double.Epsilon + double.Epsilon;
 		}
 
 		public static bool IsNearlyEqual(this double lhs, double rhs)
 		{
-			return System.Math.Abs(lhs - rhs) < double.Epsilon * double.Epsilon;
+			return System.Math.Abs(lhs - rhs) < double.Epsilon + double.Epsilon;
 		}
 
 		public static void Clamp(this ref double value, double min, double max)
--- Hazel-ScriptCore/Source/Hazel/Extension/FloatExtension.cs
+++ Hazel-ScriptCore/Source/Hazel/Extension/FloatExtension.cs
@@ -4,12 +4,12 @@
 	{
 		public static bool IsNearlyZero(this float value)
 		{
-			return System.Math.Abs(value) < float.Epsilon * float.Epsilon;
+			return System.Math.Abs(value) < float.Epsilon + float.Epsilon;
 		}
 
 		public static bool IsNearlyEqual(this float lhs, float rhs)
 		{
-			return System.Math.Abs(lhs - rhs) < float.Epsilon * float.Epsilon;
+			return System.Math.Abs(lhs - rhs) < float.Epsilon + float.Epsilon;
 		}
 
 		public static void Clamp(this ref float value, float min, float max)
